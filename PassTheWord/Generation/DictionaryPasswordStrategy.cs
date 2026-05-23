2a8fa73b916c3ceda437833aec3b15e86601ab7a
Rafi Daniël Ramharak <rafi_ramharak@hotmail.com>
2026-05-23 13:28:38 +0200

    e

--- PassTheWord/Generation/DictionaryPasswordStrategy.cs
+++ PassTheWord/Generation/DictionaryPasswordStrategy.cs
@@ -4,6 +4,8 @@ namespace PassTheWord.Generation;
 
 public class DictionaryPasswordStrategy : IPasswordGenerationStrategy
 {
+    private const int MaximumAttempts = 1000;
+
     public string Generate(PasswordOptions options)
     {
         if (!options.UseDictionary)
@@ -12,9 +14,19 @@ public class DictionaryPasswordStrategy : IPasswordGenerationStrategy
         List<string> words = options.DictionaryWords!;
 
         string password = "";
+        int attempts = 0;
 
         while (password.Length < options.MinimumLength)
         {
+            attempts++;
+
+            if (attempts > MaximumAttempts)
+            {
+                throw new InvalidOperationException(
+                    "Could not generate a dictionary password within the configured length limits."
+                );
+            }
+
             string word = words[RandomNumberGenerator.GetInt32(words.Count)];
 
             if (password.Length + word.Length > options.MaximumLength)
--- PassTheWord/Generation/RandomPasswordStrategy.cs
+++ PassTheWord/Generation/RandomPasswordStrategy.cs
@@ -6,19 +6,28 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
 {
     private const string Digits = "0123456789";
     private const string DigitsWithoutSimilar = "23456789";
-
     private const string Symbols = "!@#$%^&*()_+-=,./?~";
-
     private const string SimilarCharacters = "Il1O0";
 
     public string Generate(PasswordOptions options)
     {
         List<char> requiredCharacters = new();
 
-        string uppercaseAlphabet = BuildUppercaseAlphabet(options);
-        string lowercaseAlphabet = BuildLowercaseAlphabet(options);
-        string digitAlphabet = options.ExcludeSimilarCharacters ? DigitsWithoutSimilar : Digits;
-        string symbolAlphabet = Symbols;
+        string uppercaseAlphabet = options.UseUppercase
+            ? BuildUppercaseAlphabet(options)
+            : "";
+
+        string lowercaseAlphabet = options.UseLowercase
+            ? BuildLowercaseAlphabet(options)
+            : "";
+
+        string digitAlphabet = options.UseDigits
+            ? options.ExcludeSimilarCharacters ? DigitsWithoutSimilar : Digits
+            : "";
+
+        string symbolAlphabet = options.UseSymbols
+            ? Symbols
+            : "";
 
         if (options.RequireUppercase)
             requiredCharacters.Add(GetRandomCharacter(uppercaseAlphabet));
@@ -29,9 +38,12 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
         if (options.RequireSymbol)
             requiredCharacters.Add(GetRandomCharacter(symbolAlphabet));
 
-        string fullAlphabet = BuildFullAlphabet(options);
+        string fullAlphabet = uppercaseAlphabet + lowercaseAlphabet + digitAlphabet + symbolAlphabet;
 
-        int length = RandomNumberGenerator.GetInt32(options.MinimumLength, options.MaximumLength + 1);
+        int length = RandomNumberGenerator.GetInt32(
+            options.MinimumLength,
+            options.MaximumLength + 1
+        );
 
         List<char> passwordCharacters = [.. requiredCharacters];
 
@@ -45,28 +57,11 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
         return new string([.. passwordCharacters]);
     }
 
-    private static string BuildFullAlphabet(PasswordOptions options)
-    {
-        string alphabet = "";
-
-        if (options.UseUppercase)
-            alphabet += BuildUppercaseAlphabet(options);
-
-        if (options.UseLowercase)
-            alphabet += BuildLowercaseAlphabet(options);
-
-        if (options.UseDigits)
-            alphabet += options.ExcludeSimilarCharacters ? DigitsWithoutSimilar : Digits;
-
-        if (options.UseSymbols)
-            alphabet += Symbols;
-
-        return alphabet;
-    }
-
     private static string BuildUppercaseAlphabet(PasswordOptions options)
     {
-        string characters = string.Concat(options.Alphabets.Select(a => a.UppercaseCharacters));
+        string characters = string.Concat(
+            options.Alphabets.Select(alphabet => alphabet.UppercaseCharacters)
+        );
 
         return options.ExcludeSimilarCharacters
             ? RemoveSimilarCharacters(characters)
@@ -75,7 +70,9 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
 
     private static string BuildLowercaseAlphabet(PasswordOptions options)
     {
-        string characters = string.Concat(options.Alphabets.Select(a => a.LowercaseCharacters));
+        string characters = string.Concat(
+            options.Alphabets.Select(alphabet => alphabet.LowercaseCharacters)
+        );
 
         return options.ExcludeSimilarCharacters
             ? RemoveSimilarCharacters(characters)
@@ -84,12 +81,17 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
 
     private static string RemoveSimilarCharacters(string characters)
     {
-        return new string(characters.Where(c => !SimilarCharacters.Contains(c)).ToArray());
+        return new string(
+            characters
+                .Where(character => !SimilarCharacters.Contains(character))
+                .ToArray()
+        );
     }
 
     private static char GetRandomCharacter(string characters)
     {
         int index = RandomNumberGenerator.GetInt32(characters.Length);
+
         return characters[index];
     }
 
@@ -97,9 +99,10 @@ public class RandomPasswordStrategy : IPasswordGenerationStrategy
     {
         for (int i = characters.Count - 1; i > 0; i--)
         {
-            int j = RandomNumberGenerator.GetInt32(i + 1);
+            int randomIndex = RandomNumberGenerator.GetInt32(i + 1);
 
-            (characters[i], characters[j]) = (characters[j], characters[i]);
+            (characters[i], characters[randomIndex]) =
+                (characters[randomIndex], characters[i]);
         }
     }
 }
